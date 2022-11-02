ef1f9b0aec716e26fb74e467aa97502e1d9559e0
Yure Oliveira <yurep@amazon.com>
2022-11-02 19:28:12 +0000

    Create function to return all the values from BankAccount

--- models/bankAccount.py
+++ models/bankAccount.py
@@ -2,7 +2,6 @@ import code
 import json
 from uuid import uuid4
 
-
 class BankAccount:
   def __init__(self, companyName, codeBank, agencyNumber, accountNumber):
     self.id = uuid4().__str__()
@@ -10,6 +9,9 @@ class BankAccount:
     self.codeBank = codeBank
     self.agencyNumber = agencyNumber
     self.accountNumber = accountNumber
+  
+  def values(self):
+    return [self.id, self.companyName, self.codeBank, self.agencyNumber, self.accountNumber]
 
   def serialize(self):
     return json.dumps({
