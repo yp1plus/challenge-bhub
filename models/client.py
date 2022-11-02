19ded8f6544450ad9745a15279e7978b42de04e5
Yure Oliveira <yurep@amazon.com>
2022-11-02 19:27:44 +0000

    Add data validation

--- models/client.py
+++ models/client.py
@@ -1,13 +1,35 @@
 import json
+import phonenumbers
+from dateutil import parser
 
 class Client:
   def __init__(self, companyName, phoneNumber, address, registrationDate, invoicing):
     self.companyName = companyName
-    self.phoneNumber = phoneNumber
+    self.phoneNumber = self.validatePhoneNumber(phoneNumber)
     self.address = address
-    self.registrationDate = registrationDate
-    self.invoicing = invoicing
+    self.registrationDate = self.validateDate(registrationDate)
+    self.invoicing = self.validateNumber(invoicing)
   
+  def validatePhoneNumber(phoneNumber):
+    number = phonenumbers.parse(phoneNumber)
+    if (phonenumbers.is_valid_number(number)):
+      return phoneNumber
+    else:
+      raise ValueError("phone number is invalid")
+  
+  def validateDate(registrationDate):
+    try:
+      parser.parse(registrationDate)
+      return registrationDate
+    except ValueError:
+      raise ValueError("date format is invalid")
+  
+  def validateNumber(invoicing):
+    try:
+      return float(invoicing)
+    except ValueError:
+      raise ValueError("Invoicing is not a number")
+
   def values(self):
     return [self.companyName, self.phoneNumber, self.address, self.registrationDate, self.invoicing]
 
